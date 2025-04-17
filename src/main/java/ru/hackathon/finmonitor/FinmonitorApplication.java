5bf75c123b125e2d03db26b22989599e9507533d
Vladislav Pochernin <vspochernin@gmail.com>
2025-04-17 16:15:59 +0300

    initial commit for code

---
+++ src/main/java/ru/hackathon/finmonitor/FinmonitorApplication.java
@@ -0,0 +1,13 @@
+package ru.hackathon.finmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hackathon/finmonitor/FinmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hackathon.finmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
